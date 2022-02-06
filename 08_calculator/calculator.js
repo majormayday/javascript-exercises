91f29296d32172c11b760068b0ea28ddc4f98cdc
majormayday <minime222@gmail.com>
2022-02-06 15:45:43 -0700

    Solution to problem 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,44 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+let argLength = args.length;
+let sum =0;
+for(i=0; i<argLength; i++){ sum += args[i]}
+return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let argLength = args.length;
+let product =1;
+for(i=0; i<argLength; i++){ product *= args[i]}
+return product;
 };
 
-const power = function() {
-	
+
+const power = function(x,y) {
+let result = x
+for (i=1;i<y;i++){
+result *= x
+}
+	return result
 };
 
-const factorial = function() {
+const factorial = function(x) {
+  if(x == 0 ){
+    return 1;
+  }
+  else if (x <= 2){
+    return x;
+  }
+  else{
+    return (x * (factorial(x-1)));
+  }
 	
 };
 
