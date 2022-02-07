a40258b301d95d899ff641e0c9946524a21e1d28
majormayday <minime222@gmail.com>
2022-02-06 22:54:59 -0700

    Test of prettier extension.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,15 @@
-const findTheOldest = function(people) {
+const findTheOldest = function (people) {
   let todayDate = new Date();
   let todayYear = todayDate.getFullYear();
 
-  let oldest = people.reduce(((prev,curr) => {
-    if ((curr.yearOfDeath - curr.yearOfBirth) > ((prev?.yearOfDeath ?? todayYear) - prev?.yearOfBirth)) {return curr}
-    else {return prev};
+  let oldest = people.reduce(((prev, curr) => {
+    if ((curr.yearOfDeath - curr.yearOfBirth) > ((prev?.yearOfDeath ?? todayYear) - prev?.yearOfBirth)) { return curr }
+    else { return prev };
   }))
   return oldest;
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
