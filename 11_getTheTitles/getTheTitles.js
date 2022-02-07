731f20bde89df2e0c06a2a7efc5a939f92aff26e
majormayday <minime222@gmail.com>
2022-02-06 20:43:15 -0700

    SOlution to question 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let onlyBooks = books.map(book => {
+        for (let key in book){
+            if (key == 'title'){
+                return book[key]
+            }
+        }
+    });
+    return onlyBooks;
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
