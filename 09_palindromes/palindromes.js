198b1b04c8b296875a1f55d39ab03773604b8829
majormayday <minime222@gmail.com>
2022-02-06 20:13:38 -0700

    Solution to question 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g;
+    let cleanString = string.replace(regex,"");
+    let splitString = cleanString.split('');
+    let len = splitString.length;
+    let flippedString = [];
 
+    for (i=0; i < len;i++){
+        let popped = splitString.pop();
+        flippedString.push(popped);
+    }
+
+    if(flippedString.join("").toLowerCase() == cleanString.toLowerCase()){
+     return true
+    }
+    else {return false}
 };
-
 // Do not edit below this line
 module.exports = palindromes;
