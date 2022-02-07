dd215fd23c184d5adf816786213a14df707024f3
majormayday <minime222@gmail.com>
2022-02-06 22:30:46 -0700

    Solution to problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  let todayDate = new Date();
+  let todayYear = todayDate.getFullYear();
 
+  let oldest = people.reduce(((prev,curr) => {
+    if ((curr.yearOfDeath - curr.yearOfBirth) > ((prev?.yearOfDeath ?? todayYear) - prev?.yearOfBirth)) {return curr}
+    else {return prev};
+  }))
+  return oldest;
 };
 
 // Do not edit below this line
